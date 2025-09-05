f3c3e6014d1e1fc742cc8d4ab5c18a026bf45535
Varshiha <varshihaa@icloud.com>
2025-09-04 23:33:37 -0400

    I added a new class named Date with parameters.

---
+++ Date.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class Date here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Date
+{
+    // instance variables - replace the example below with your own
+    private String month; 
+    private int day;
+    private int year;
+
+    /**
+     * Constructor for objects of class Date
+     */
+    public Date(String month, int day, int year)
+    {
+        // initialise instance variables
+        this.month = month;
+        this.day = day; 
+        this.year = year;
+        
+    }
+
+}
--- TicketMachine.java
+++ TicketMachine.java
@@ -76,3 +76,4 @@ public class TicketMachine
         balance = 0;
     }
 }
+
