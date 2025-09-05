a29c73197a6c75c711bf1bed886d5ed1239570f6
Varshiha <varshihaa@icloud.com>
2025-09-04 23:56:27 -0400

    I added getTotal.

--- TicketMachine.java
+++ TicketMachine.java
@@ -29,7 +29,10 @@ public class TicketMachine
         balance = 0;
         total = 0;
     }
-
+    public int getTotal()
+    {
+        return total;
+    }
     /**
      * Return the price of a ticket.
      */
